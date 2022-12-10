4723945adc0fd04f60de1b959274ffe71b63b579
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-12-10 15:15:27 -0300

    feature: database folder have TypeOrmConfig to connect with DB

---
+++ src/database/typeorm.config.ts
@@ -0,0 +1,32 @@
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModuleAsyncOptions, TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+
+export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
+  useFactory: async (config: ConfigService): Promise<TypeOrmModuleOptions> => ({
+    type: 'mysql',
+    host: config.get('DB_HOST'),
+    port: Number(config.get('DB_PORT')),
+    database: config.get('MYSQL_DATABASE'),
+    username: config.get('MYSQL_USER'),
+    password: config.get('MYSQL_ROOT_PASSWORD'),
+    migrations: ['/dist/migrations/*.{ts,js}'],
+    logging: true,
+    synchronize: true, // never use TRUE in production!
+    autoLoadEntities: true,
+  }),
+  inject: [ConfigService],
+};
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.MYSQL_DATABASE,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  migrations: ['/dist/migrations/*.{ts,js}'],
+  logging: true,
+  synchronize: true, // never use TRUE in production!
+  autoLoadEntities: true,
+};
